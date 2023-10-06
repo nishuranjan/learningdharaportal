a2991e489da588738840f9b4b3706f7b2d7c9ab4
nishuranjan <nishantcont@gmail.com>
2023-10-06 20:34:35 +0530

    created api poc

---
+++ backend/fastapi/books.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+
+@app.get("/")
+async def first_api():
+    return {"message": "Hello Nishant!"}
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS 
