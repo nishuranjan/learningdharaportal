8b83c07aea43a85920e22ff5a1e83e29b858785f
nishuranjan <nishantcont@gmail.com>
2023-10-10 19:48:23 +0530

    added complete poc on fastapi

--- backend/api/books.py
+++ backend/api/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -20,4 +20,75 @@ async def first_api():
 
 @app.get("/books")
 async def read_all_books():
-    return BOOKS 
+    return BOOKS
+
+
+@app.get("/books/mybook")
+async def read_all_books():
+    return {'book_title': 'My Favorite Book !'}
+
+
+@app.get("/books/{book_title}")
+async def read_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
+# @app.get("/books/{dynamic_param}")
+# async def read_all_books(dynamic_param):
+#     return {'dynamic_param': dynamic_param}
+
+
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def create_book(update_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book\
+                                                    .get('title')\
+                                                    .casefold():
+            BOOKS[i] = update_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title\
+                                                    .casefold():
+            BOOKS.pop(i)
+            break
