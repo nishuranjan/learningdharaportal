918161c8546fa1d6a6a80140a7b118fc3373375c
bineetatdev <bineet.nitc@gmail.com>
2023-10-10 17:00:58 +0530

    Added webpack and index

--- frontend/app/Main.js
+++ frontend/app/Main.js
@@ -1,16 +1,19 @@
 import React, { useEffect } from "react"
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import "./assets/css/styles.css"
+
+import "./assets/js/jquery.min.js"
+import Home from "./components/Home"
+import Footer from "./components/Footer"
+import Modal from "./components/Modal"
 
 function Main() {
-  return (
-    <div>
-      <h1>This is our test app</h1>
-      <p>Setting up new project</p>
-    </div>
-  )
+  return <></>
 }
 
-const root = ReactDOM.createRoot(document.querySelector("#app"))
+const root = ReactDOM.createRoot(document.querySelector("#main-wrapper"))
 root.render(<Main />)
 if (module.hot) {
   module.hot.accept()
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -30,6 +30,22 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/", // Output path for the images
+            },
+          },
+        ],
+      },
     ],
   },
 }
